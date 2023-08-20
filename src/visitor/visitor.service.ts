f10bb3f673cb5ccb88e74c1b7272bdca40886e0a
Sanjida Akter <sanjidakter2304@gmail.com>
2023-08-20 20:34:10 +0600

    visitor count gets updated within same row

--- src/visitor/visitor.service.ts
+++ src/visitor/visitor.service.ts
@@ -1,5 +1,5 @@
 import { Repository } from 'typeorm';
-import { Visitor } from './visitor.entity'; // Import your Visitor entity
+import { Visitor } from './visitor.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -11,32 +11,36 @@ export class VisitorService {
   ) {}
 
   async incrementVisitorCount(): Promise<void> {
-    const newVisitor = new Visitor();
-    newVisitor.visitorCount = await this.calculateNewVisitorCount();
-
-    await this.visitorRepository.save(newVisitor);
-  }
-
-  async getVisitorCount(): Promise<number> {
-    const allVisitors = await this.visitorRepository.find();
-  
-    if (allVisitors.length === 0) {
-      return 0; // Return 0 if no visitors are found
+    try {
+      // Use TypeORM query builder to increment the visitorCount
+      await this.visitorRepository
+        .createQueryBuilder()
+        .update(Visitor)
+        .set({ visitorCount: () => "visitorCount + 1" })
+        .execute();
+    } catch (error) {
+      // Handle error (e.g., if the record does not exist)
+      throw error;
     }
-  
-    return allVisitors[allVisitors.length - 1].visitorCount;
   }
   
+  async getVisitorCount(): Promise<number> {
+    try {
+      // Use TypeORM query builder to retrieve the visitorCount
+      const result = await this.visitorRepository
+        .createQueryBuilder()
+        .select("visitorCount")
+        .getRawOne();
   
-
-  private async calculateNewVisitorCount(): Promise<number> {
-    const maxVisitorCount = await this.visitorRepository
-      .createQueryBuilder('visitor')
-      .select('MAX(visitor.visitorCount)', 'maxCount')
-      .getRawOne();
-
-    const newDefaultVisitorCount = maxVisitorCount.maxCount + 1;
-
-    return newDefaultVisitorCount;
+      if (result) {
+        return result.visitorCount;
+      } else {
+        return 0; // If no visitor record exists, return 0
+      }
+    } catch (error) {
+      // Handle error (e.g., if the record does not exist)
+      throw error;
+    }
   }
+  
 }
